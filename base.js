c0476dd168a31dd4f993ad8ecf1de8ab4cb743b3
Ojogun Emmanuel Ehiedu <39580043+ojogunemmy@users.noreply.github.com>
2023-05-20 17:42:35 +0100

    Update base.js

--- base.js
+++ base.js
@@ -5,7 +5,7 @@
 var listData = [1,2,3,4,5,6]
 document.querySelector(".value1").value=0
 
-function playerOne(){
+function playerOne(){ 
     var randomNum =Math.round(Math.random()*6);
     var dice = ["images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png"]
     
